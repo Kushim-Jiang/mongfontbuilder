e456c5c6177aad5bfe50da3c9fa38ce99a6d08aa
Liang Hai <lianghai@gmail.com>
2024-12-22 14:09:37 +0100

    Revise data structure
    - Identify variants under a position with FVS assignment.
    - Recognize localization of certain variants.

--- lib/mongfontbuilder/data/convert.py
+++ lib/mongfontbuilder/data/convert.py
@@ -58,26 +58,27 @@ for locale, gbNumber in {
         _ = value.pop("transcription", None)
         assert not value, value
 
-        newValue = newData.get(cp)
-        if not newValue:
-            newValue = {"alias": {}}
-            newData[cp] = newValue
+        newValue = newData[cp]
         newValue["alias"][locale] = id
 
         if variants:
-            newVariants = newValue.setdefault("variants", {})
+            newVariants = newValue["variants"]
             for position in joiningPositions:
                 for variant in variants.pop(position):
+                    localeData = {}
+                    newVariantsOnPosition = newVariants.get(position, [])
                     written = variant.pop("written_units")
                     try:
                         newVariant = next(
-                            i for i in newVariants.get(position, []) if i["written"] == written
+                            i for i in newVariantsOnPosition if i["written"] == written
                         )
                     except StopIteration:
-                        newVariant = {"written": written}
-                        newVariants.setdefault(position, []).append(newVariant)
+                        localizingTarget = variant.pop("localizing", 0)
+                        newVariant = next(
+                            i for i in newVariantsOnPosition if i.get("fvs", 0) == localizingTarget
+                        )
+                        localeData["written"] = written
 
-                    localeData = {}
                     newVariant.setdefault("locales", {}).setdefault(locale, localeData)
                     if conditions := variant.pop("conditions", None):
                         localeData["conditions"] = [i.removeprefix(".") for i in conditions]
@@ -91,33 +92,46 @@ for locale, gbNumber in {
                     assert not variant, variant
             assert not variants, variants
 
+
+def normalizeWritten(written: list[str]) -> list[str]:
+    written = [i.removeprefix(".") for i in written]
+    if "." in written[0]:
+        [*written], [position, *trailingPositions] = zip(*[i.split(".") for i in written])
+        for trailingPosition in trailingPositions:
+            position = {
+                "init": {
+                    "medi": "init",
+                    "fina": "isol",
+                },
+                "medi": {
+                    "medi": "medi",
+                    "fina": "fina",
+                },
+            }[position][trailingPosition]
+        written.append(position)
+    return written
+
+
 normalizedData = {}
 for cp, value in sorted(newData.items()):
-    normalizedData[unicodedata.name(chr(cp))] = value
+    name = unicodedata.name(chr(cp))
+    normalizedData[name] = value
     alias = {k: v.removeprefix(".") for k, v in value["alias"].items() if v}
     if len({*alias.values()}) == 1:
         alias, *_ = alias.values()
     value["alias"] = alias
     if variants := value.get("variants"):
         for position in joiningPositions:
+            fvsToVariant = dict[str, dict]()
             for variant in variants[position]:
-                written = [i.removeprefix(".") for i in variant["written"]]
-                if "." in written[0]:
-                    [*written], [position, *trailingPositions] = zip(
-                        *[i.split(".") for i in written]
-                    )
-                    for trailingPosition in trailingPositions:
-                        position = {
-                            "init": {
-                                "medi": "init",
-                                "fina": "isol",
-                            },
-                            "medi": {
-                                "medi": "medi",
-                                "fina": "fina",
-                            },
-                        }[position][trailingPosition]
-                    written.append(position)
-                variant["written"] = written
+                key = str(variant.pop("fvs", 0))
+                assert key not in fvsToVariant, (name, position, fvsToVariant, key)
+                fvsToVariant[key] = variant
+                variant["written"] = normalizeWritten(variant["written"])
+                for locale, localeData in variant["locales"].items():
+                    if written := localeData.get("written"):
+                        localeData["written"] = normalizeWritten(written)
+            # variants[position] = dict(sorted(fvsToVariant.items()))
+            variants[position] = fvsToVariant
 
 (dir / "characters.json").write_text(json.dumps(normalizedData, indent=2))
