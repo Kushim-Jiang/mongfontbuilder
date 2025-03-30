93d7c8119c8b2cc87b3b005778d423c3cd38e31d
Liang Hai <lianghai@gmail.com>
2025-03-30 10:27:15 +0200

    Simplify typing; update dependencies

--- lib/mongfontbuilder/data/misc.py
+++ lib/mongfontbuilder/data/misc.py
@@ -1,9 +1,5 @@
-from typing import Literal, cast, get_args
+from typing import Literal, get_args
 
 JoiningPosition = Literal["isol", "init", "medi", "fina"]
-joiningPositions: tuple[JoiningPosition] = get_args(JoiningPosition)
-
-isol = cast(JoiningPosition, "isol")
-init = cast(JoiningPosition, "init")
-medi = cast(JoiningPosition, "medi")
-fina = cast(JoiningPosition, "fina")
+joiningPositions: list[JoiningPosition] = [*get_args(JoiningPosition)]
+isol, init, medi, fina = joiningPositions
