4725a6ea4be3731cac9b99c5ed90504d1a0ae60d
Liang Hai <lianghai@gmail.com>
2025-05-04 16:37:56 +0800

    Remove unused test file

--- tests/main.py
+++ tests/main.py
@@ -4,23 +4,41 @@ from subprocess import run
 
 from fontTools.ttLib import TTFont
 from ufo2ft import OTFCompiler
+from ufo2ft.constants import CFFOptimization
 from ufoLib2 import Font
 from utils import tempDir, testsDir
 
+input = testsDir / "hudum.ufo"
+intermediate = tempDir / "build.ufo"
+
 
 def main() -> None:
-    output = tempDir / "build.ufo"
     run(
         ["poetry", "run", "python", "-m", "mongfontbuilder"]
-        + [testsDir / "hudum.ufo", output, "--locales", "MNG"]
+        + [input, intermediate, "--locales", "MNG"]
     )
 
+    compiler = OTFCompiler(
+        useProductionNames=False,
+        optimizeCFF=CFFOptimization.NONE,
+    )
     environ["FONTTOOLS_LOOKUP_DEBUGGING"] = "1"  # For feaLib.builder.Builder
-    font: TTFont = OTFCompiler(useProductionNames=False).compile(Font.open(output))
+    font: TTFont = compiler.compile(Font.open(intermediate))
 
-    output = output.with_suffix(".otf")
+    output = tempDir / makeFontFilename(font)
     font.save(output)
     print(relpath(output))
+
+
+def makeFontFilename(font: TTFont) -> str:
+    from fontTools.ttLib.tables._n_a_m_e import table__n_a_m_e
+
+    table: table__n_a_m_e = font["name"]  # type: ignore
+    postScriptName = table.getDebugName(6)
+    assert postScriptName
+
+    suffix = ".otf" if {"CFF ", "CFF2"}.intersection(font.keys()) else ".ttf"
+    return postScriptName + suffix
 
 
 if __name__ == "__main__":
