2fddf0f1f1e39895ac41e63753c1cfac34e00211
梁海 Liang Hai <lianghai@gmail.com>
2026-05-17 02:55:38 +0200

    Template for Glyphs
    Clean up the Glyphs file to minimize noises.

---
+++ templates/update.py
@@ -0,0 +1,94 @@
+from pathlib import Path
+
+from glyphsLib import GSFeaturePrefix, GSFont, GSFontMaster, GSGlyph, GSLayer
+from glyphsLib.builder import GlyphsBuilder
+from ufoLib2 import Font
+
+from mongfontbuilder.otl import MongFeaComposer
+from mongfontbuilder.spec import applySpecToFont
+
+templatesDir = Path(__file__).parent
+repo = templatesDir / ".."
+
+
+def main() -> None:
+    font = Font.open(repo / "tests" / "hudum.ufo")
+    font._path = None  # glyphsLib expects str and may load from path
+
+    composer = MongFeaComposer(
+        cmap={j: i for i in font.keys() for j in font[i].unicodes},
+        glyphs=[*font.keys()],
+        locales=["MNG"],
+    )
+    composer.languageSystems["DFLT"] = {"dflt"}
+    spec = composer.compose()
+    fea = composer.asFeatureFile().asFea()
+
+    applySpecToFont(  # Padding disturbs automatic alignment in Glyphs
+        spec, font, initPadding=0, finaPadding=0
+    )
+    font.glyphOrder = [
+        i
+        for i in font.glyphOrder
+        if i
+        not in [
+            ".notdef",
+            "mvs",
+            "mvs.narrow",
+            "mvs.wide",
+            "fvs1",
+            "fvs2",
+            "fvs3",
+            "fvs4",
+            "zwj",
+            "zwnj",
+            "space",
+            "nnbsp",
+        ]
+    ]
+    i = font.info
+    assert i.copyright
+    i.copyright = "Glyph outlines in this template: " + i.copyright
+    i.familyName, i.styleName = "Hudum Template", "Regular"
+    i.styleMapFamilyName, i.styleMapStyleName = None, None
+    i.postscriptFontName = None
+    i.ascender, i.capHeight, i.xHeight, i.descender = 800, 700, 500, -200
+    i.openTypeVheaVertTypoAscender, i.openTypeVheaVertTypoDescender = None, None
+    i.openTypeHeadFlags = None
+    i.openTypeHeadLowestRecPPEM = None
+
+    gsFont: GSFont = GlyphsBuilder(ufos=[font]).font
+    gsFont.axes.clear()
+    master: GSFontMaster
+    [master] = gsFont.masters
+    master.axes.clear()
+    prefixes: list[GSFeaturePrefix] = gsFont.featurePrefixes
+    prefixes.append(GSFeaturePrefix("mongfontbuilder", "include(hudum.fea);"))
+    gsFont.disablesNiceNames = True
+    gsFont.format_version = 3
+    glyph: GSGlyph
+    for glyph in gsFont.glyphs:
+        if glyph.name in ["mvs.narrow", "mvs.wide", "fvs4"]:
+            glyph.script = "mongolian"
+        if glyph.name in ["mvs.narrow", "mvs.wide"]:
+            glyph.category, glyph.subCategory = "Separator", "Space"
+        if glyph.name in ["mvs", "fvs1", "fvs2", "fvs3", "fvs4", "zwnj", "zwj", "nnbsp"]:
+            glyph.category, glyph.subCategory = "Separator", "Other"
+        glyph.note = None
+        layer: GSLayer
+        [layer] = glyph.layers
+        if not len(layer.paths):
+            if layer.components:
+                glyph.color = 10  # Light gray
+            elif not layer.width:
+                glyph.color = 11  # Dark gray
+        layer.vertWidth = None
+
+    (templatesDir / "hudum.fea").write_text(
+        fea.replace(":", ".")  # Glyphs doesn't support ":" in lookup names
+    )
+    gsFont.save(templatesDir / "hudum.glyphs")
+
+
+if __name__ == "__main__":
+    main()
