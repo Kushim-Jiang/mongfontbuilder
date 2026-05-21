a791b62f400011466bfdcae52f05c2ada3b108e9
梁海 Liang Hai <lianghai@gmail.com>
2026-05-21 04:36:26 +0200

    Avoid unintentional call of loadTestCases()

--- tests/test_templates.py
+++ tests/test_templates.py
@@ -9,16 +9,16 @@ from utils import repo, tempDir
 targeted = sys.platform == "darwin"
 if targeted:
     run(["uv", "run", "glyphs", "export", "--output", tempDir, repo / "templates" / "hudum.glyphs"])
-
-
-@mark.skipif(not targeted, reason="The test font can only be built on macOS.")
-@mark.parametrize(
-    ("codes", "index", "result", "goal"),
-    loadTestCases(
+    testCases = loadTestCases(
         tempDir / "HudumTemplate-Regular.otf",
         {"eac": ["hud"], "core": ["hud"]},
         "MNG",
-    ),
-)
+    )
+else:
+    testCases = []
+
+
+@mark.skipif(not targeted, reason="The test font can only be built on macOS.")
+@mark.parametrize(("codes", "index", "result", "goal"), testCases)
 def test_MNG(codes: str, index: str, result: str, goal: str) -> None:
     assert result == goal, f"ind:  {index}\ncode: {codes}\nres:  {result}\ngoal: {goal}"
