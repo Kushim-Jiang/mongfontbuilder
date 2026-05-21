f0e708cfff140a05d411169abe46bf9f424e9143
梁海 Liang Hai <lianghai@gmail.com>
2026-05-21 03:09:38 +0200

    Build the Glyphs template’s test font using glyphs-cli
    glyphsLib + ufo2ft can’t replicate how Glyphs decides GDEF classes.

--- tests/test_templates.py
+++ tests/test_templates.py
@@ -1,27 +1,21 @@
-import pytest
-from glyphsLib import GSFont, to_ufos
-from ufoLib2 import Font
+import sys
+from subprocess import run
 
-from fixtures import compileOTF, loadTestCases
+from pytest import mark
+
+from fixtures import loadTestCases
 from utils import repo, tempDir
 
-font: Font
-[font] = to_ufos(  # type: ignore
-    GSFont(repo / "templates" / "hudum.glyphs"),
-    write_skipexportglyphs=True,
-    expand_includes=True,
-    minimal=True,
-)
-font.save(tempDir / "hudum-template.ufo", overwrite=True)
-ttFont = compileOTF(font)
-path = tempDir / "hudum-template.otf"
-ttFont.save(path)
+targeted = sys.platform == "darwin"
+if targeted:
+    run(["uv", "run", "glyphs", "export", "--output", tempDir, repo / "templates" / "hudum.glyphs"])
 
 
-@pytest.mark.parametrize(
+@mark.skipif(not targeted, reason="The test font can only be built on macOS.")
+@mark.parametrize(
     ("codes", "index", "result", "goal"),
     loadTestCases(
-        path,
+        tempDir / "HudumTemplate-Regular.otf",
         {"eac": ["hud"], "core": ["hud"]},
         "MNG",
     ),
