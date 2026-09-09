cfa24a644e00c8415b58c2bf2fc7f65b6f5158fe
Kushim-Jiang <894857367@qq.com>
2026-09-09 15:20:20 +0800

    Remove space

--- web/lib/Punc.svelte
+++ web/lib/Punc.svelte
@@ -10,15 +10,14 @@
   const left = $derived(String.fromCodePoint(punctuation.Boundaryleft.code));
   const right = $derived(String.fromCodePoint(punctuation.Boundaryright.code));
   const unicode = $derived("unicode" in entry ? entry.unicode : undefined);
-  const space = $derived("space" in entry ? entry.space : false);
   const mark = $derived(String.fromCodePoint(entry.code));
   const codepoint = $derived(unicode === undefined ? "" : `U+${unicode.toString(16).toUpperCase().padStart(4, "0")}`);
 </script>
 
-<td>{codepoint}{space ? " (U+0020)" : ""}</td>
+<td>{codepoint}</td>
 <td>
   <span class="wu wu-rotated">
-    <span class="wu-gray">{left}</span><span class="wu-box">{mark}</span>{#if space}<span class="wu-box">{" "}</span>{/if}<span class="wu-gray">{right}</span>
+    <span class="wu-gray">{left}</span><span class="wu-box">{mark}</span><span class="wu-gray">{right}</span>
   </span>
 </td>
 
